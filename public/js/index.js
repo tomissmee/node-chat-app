f0a27013a7ac55e07a1094448f1222027bb80414
tomissmee <tommyvong88@gmail.com>
2017-09-25 12:20:52 -0700

    Emit newMessage on createNewMessage

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,13 @@ socket.on('connect', function() {
     console.log('connected to server');
 });
 
-socket.on('disconnect', function() {
-    console.log('disconnected form server');
+socket.on('disconnect', function(){
+    console.log('Disconnect from server');
 });
 
-socket.on('newMessage', function(message){
-    console.log('newMessage', message);
-})
+socket.on('newMessage', function (message) {
+    console.log('New message', message);
+});
+
+
+
