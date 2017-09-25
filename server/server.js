0c6a9d304ec97e214e8880829cf1be11c97032b4
tomissmee <tommyvong88@gmail.com>
2017-09-25 12:56:24 -0700

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -16,13 +16,22 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    //socket.emit from admin text welcome to chat app
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to chat app!',
+        createdAt: new Date().getTime()
+    });
+
+    //socket.broadcast.emit from admin text new user joined
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'new user joined',
+        createdAt: new Date().getTime()
+    });            
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
     });
 
     socket.on('disconnect', () => {
